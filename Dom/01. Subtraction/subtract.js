fb6e37da1a2edab6b34edd751d79821fcd65dc93
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2020-12-27 19:06:27 +0200

    Web Exercise

--- Dom/01. Subtraction/subtract.js
+++
@@ -1,6 +0,0 @@
-function subtract() {
-    let firstElement = document.getElementById('firstNumber');
-    let secondElement = document.getElementById('secondNumber');
-    let res = document.getElementById('result');
-    res.innerHTML = Number(firstElement.value) - Number(secondElement.value)
-}
