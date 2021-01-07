1547d28844cb4bf60573261b9cfcbdf7beba82e3
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2021-01-07 19:13:46 +0200

    Web exercise

--- Dom Manipulations/17.Food-Description-App/doc.js
+++ Dom Manipulations/17.Food-Description-App/doc.js
@@ -1,4 +1,5 @@
 function cart() {
+
     let buttonInbox = document.getElementsByTagName('button');
     let arrayInboxBtn = Array.from(buttonInbox);
     let inboxContainer = document.getElementsByClassName('inbox')[0]
@@ -8,40 +9,36 @@ function cart() {
 
     let recMain = document.getElementsByClassName('recipe-main')[0]
 
+    let descriptElement = document.getElementsByTagName('p');
+    let arrayDescriptElement = Array.from(descriptElement);
 
     let array = [];
 
     for (let i = 0; i < arrayInboxBtn.length; i++) {
         arrayInboxBtn[i].addEventListener('click', function (e) {
-            let currentElement = e.currentTarget.parentElement;
+            let currentElement = e.target.parentElement;
 
             let heading = currentElement.children[0].innerText;
             let img = currentElement.children[1].src
-            let descript = currentElement.children[2].innerText;
-
-            array.push(heading, img, descript);
+            array.push(heading, img, );
 
             let sectionElement = document.createElement('section');
             let headingElement = document.createElement('h1');
             let imgPar = document.createElement('img');
-            let des = document.createElement('p');
-            let removeButton=document.createElement('button');
-            removeButton.className='delete'
-            removeButton.textContent='Remove';
+            let removeButton = document.createElement('button');
+            removeButton.className = 'delete'
+            removeButton.textContent = 'Remove';
 
             sectionElement.className = 'product';
-
+            imgPar.src = img;
             headingElement.textContent = heading
-            imgPar.src = `${img}`
-            des.textContent = descript
-            
+
             sectionElement.appendChild(headingElement)
             sectionElement.appendChild(imgPar);
-            sectionElement.appendChild(des);
             sectionElement.appendChild(removeButton)
 
-            removeButton.addEventListener('click',function(){
-                let parent=removeButton.parentNode;
+            removeButton.addEventListener('click', function () {
+                let parent = removeButton.parentNode;
                 parent.remove();
             })
 
@@ -53,10 +50,22 @@ function cart() {
         liElement[j].addEventListener('click', function () {
             ulLement.getElementsByClassName('active')[0].classList.remove('active');
             liElement[j].classList.add('active');
-
             recMain.getElementsByClassName('active')[0].classList.remove('active');
             recMain.getElementsByTagName('div')[j].classList.add('active')
         })
     }
 
+    arrayDescriptElement.forEach((value, index) => {
+        value.addEventListener('click', function (e) {
+            let parElement = e.currentTarget.parentElement;
+            let spanElement = parElement.children[3];
+            if (value.textContent == 'description') {
+                spanElement.style.display = 'block'
+                value.innerHTML = 'Hide description'
+            } else if (value.textContent == 'Hide description') {
+                spanElement.style.display = 'none';
+                value.innerHTML = 'description'
+            }
+        })
+    })
 }
