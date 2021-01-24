58d51e9814ce97efd79c43119c37ea840f9b1434
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2021-01-24 10:44:31 +0200

    Added users

--- 4.Asynchronous Programming Projects/2.Get Users/doc.js
+++ 4.Asynchronous Programming Projects/2.Get Users/doc.js
@@ -2,32 +2,19 @@ function getUsers() {
     let url = `https://jsonplaceholder.typicode.com/users`
     let usersContainer = document.getElementsByClassName('getUsers')[0]
     let output = '';
-    let userSearch=document.getElementsByTagName('button')[0];
-
     fetch(url)
         .then((response) => {
             return response.json()
         }).then((data) => {
             data.forEach(element => {
-                console.log(element)
-
-                output +=`<div class="user">`+
+                output += `<div class="user">` +
                     `<p>ID: ${element.id}</p>` +
                     `<p>Name: ${element.name}</p>` +
-                    `<p>Username: ${element.username}</p>` + 
-                    `<p>${element.email}</p>`+
+                    `<p>Username: ${element.username}</p>` +
+                    `<p>${element.email}</p>` +
                     `</div>`
             });
-            usersContainer.innerHTML=output
+            usersContainer.innerHTML = output
         })
-
-        userSearch.addEventListener('click',function(e){
-            e.preventDefault();
-            let inputElement=document.getElementsByTagName('input')[0]
-            let url = `https://jsonplaceholder.typicode.com/${inputElement.value}`
-            
-        })
-
-
 }
 getUsers()
