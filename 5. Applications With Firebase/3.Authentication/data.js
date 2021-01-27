6a71b671fe5a8b880ff161b03ff89b7efbed6e0f
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2021-01-27 16:41:47 +0200

    Authentication Firebase Register and Login Form

---
+++ 5. Applications With Firebase/3.Authentication/data.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAOGoz-M2iDAmjIRYAlk2rM7dS9fiKJF-g",
+    authDomain: "authentication-a030b.firebaseapp.com",
+    projectId: "authentication-a030b",
+    storageBucket: "authentication-a030b.appspot.com",
+    messagingSenderId: "645207657513",
+    appId: "1:645207657513:web:82401c8b6062f166e1e038",
+    measurementId: "G-XGF6X0G15C"
+  };
+  firebase.initializeApp(firebaseConfig);
+
