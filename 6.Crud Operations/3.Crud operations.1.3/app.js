81c14e4ecb945267385b7fcdb10eabdb832d789b
georgiangelov2000 <angelov2000@abv.bg>
2021-01-29 00:10:56 +0200

    Edit Remove Add Operations

---
+++ 6.Crud Operations/3.Crud operations.1.3/app.js
@@ -0,0 +1,24 @@
+const ul = document.getElementById('list');
+
+ul.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        const button = event.target;
+        const li = button.parentNode;
+        if (button.textContent === 'edit') {
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span)
+            button.textContent = 'save'
+        } else if (button.textContent === 'save') {
+            const input = li.firstElementChild;
+            const span = document.createElement('span');
+            span.textContent = input.value;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'edit'
+        }
+    }
+})
