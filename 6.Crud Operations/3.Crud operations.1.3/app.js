e11348bf05c54dc78a93b7adb054ee7a62f695ab
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2021-01-31 14:55:12 +0200

    Update Crud Operation

--- 6.Crud Operations/3.Crud operations.1.3/app.js
+++ 6.Crud Operations/3.Crud operations.1.3/app.js
@@ -1,4 +1,30 @@
 const ul = document.getElementById('list');
+let form = document.getElementById('myForm');
+
+form.addEventListener('submit', sendMessage);
+
+function sendMessage(e) {
+    e.preventDefault();
+    let textInput = document.getElementById('myText').value;
+    if (textInput === '') {
+       alert('Enter the input please!')
+    } else {
+        const liElement = document.createElement('li')
+        const spanElement = document.createElement('span')
+        const editElement = document.createElement('button')
+        const deleteElement = document.createElement('button')
+        editElement.textContent = 'edit';
+        deleteElement.textContent = 'remove';
+        spanElement.textContent = textInput;
+        liElement.appendChild(spanElement);
+        liElement.appendChild(editElement);
+        liElement.appendChild(deleteElement);
+        document.getElementById('list').appendChild(liElement);
+        form.reset();
+
+    }
+}
+
 
 ul.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON') {
@@ -19,6 +45,8 @@ ul.addEventListener('click', (event) => {
             li.insertBefore(span, input);
             li.removeChild(input);
             button.textContent = 'edit'
+        } else if (button.textContent === 'remove') {
+            ul.removeChild(li);
         }
     }
 })
