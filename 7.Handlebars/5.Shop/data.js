fc76abb06bc47194884226fe3735efe1705ed9cb
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2021-02-02 18:23:33 +0200

    Added Shop Handlebars Project

---
+++ 7.Handlebars/5.Shop/data.js
@@ -0,0 +1,45 @@
+const products=[
+    {
+        id:1,
+        name:'Ultraboost 20 Men Running Shoes',
+        price:'90$',
+        imgSrc:'/images/Ultraboost 20 Men Running Shoes.jpeg',
+        buttonInfo:'Click Me'
+    },
+    {
+        id:2,
+        name:'TERREX Trailmaker Men Shoes',
+        price:'80$',
+        imgSrc:'/images/TERREX Trailmaker Men Shoes.jpeg',
+        buttonInfo:'Click Me'
+    },
+    {
+        id:3,
+        name:'TERREX Swift R2 GORE-TEX Men Approach Shoes',
+        price:'50$',
+        imgSrc:'/images/TERREX Swift R2 GORE-TEX Men Approach Shoes.jpeg',
+        buttonInfo:'Click Me'
+    },
+    {
+        id:4,
+        name:'Flif Print T High Rise Men Tennis Top',
+        price:'30$',
+        imgSrc:'/images/Flif Print T High Rise Men Tennis Top.jpeg',
+        buttonInfo:'Click Me'
+    },
+    {
+        id:5,
+        name:'2in1 Ergo Heat Men Tennis Shorts',
+        price:'20$',
+        imgSrc:'/images/2in1 Ergo Heat Men Tennis Shorts.jpeg',
+        buttonInfo:'Click Me'
+    },
+    {
+        id:6,
+        name:'TERREX Two Ultra Parley Men TrailRunning Shoes',
+        price:'95$',
+        imgSrc:'/images/TERREX Two Ultra Parley Men Trailrunning Shoes.jpeg',
+        buttonInfo:'Click Me'
+    },
+]
+export default products
