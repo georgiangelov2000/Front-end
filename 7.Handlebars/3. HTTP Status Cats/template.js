ea4df67b9eb473980365c3617b3a9cbd11807353
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2021-02-02 16:25:42 +0200

    Cats Handlebars project

---
+++ 7.Handlebars/3. HTTP Status Cats/template.js
@@ -0,0 +1,35 @@
+const elements={
+    allCats: () => document.getElementById('allCats')
+};
+
+Promise.all([
+    getTemplate('./template.hbs'),
+    getTemplate('./cat.hbs')
+]).then(([templateSrc,catSrc])=>{
+    Handlebars.registerPartial('cat',catSrc);
+    let template=Handlebars.compile(templateSrc);
+    let resultHTML=template({cats});
+    elements.allCats().innerHTML=resultHTML;
+    attachEventListener();
+})
+.catch((e)=>console.error(e))
+
+
+function getTemplate(templateLocation){
+    return fetch(templateLocation).then((r)=>r.text());
+}
+function attachEventListener(){
+    elements.allCats().addEventListener('click',(e)=>{
+        const{target}=e;
+        if(target.nodeName==='BUTTON' ){
+            let divStatus=target.parentNode.querySelector('div.status');
+            if(divStatus.style.display==='block'){
+                divStatus.style.display='none';
+                e.target.textContent='Show status code';
+            }else{
+                divStatus.style.display='block';
+                e.target.textContent='Hide status code';
+            }
+        }
+    })
+}
